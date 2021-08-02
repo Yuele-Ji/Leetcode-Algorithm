6f17a9f8511c6be14fa782a0c7212a1d5f7a40e4
Yuele-Ji <80063568+Yuele-Ji@users.noreply.github.com>
2021-08-02 02:52:22 -0400

    Update 141. Linked List Cycle.py

--- 141. Linked List Cycle.py
+++ 141. Linked List Cycle.py
@@ -2,10 +2,22 @@
 class Solution:
     def hasCycle(self, head:ListNode) -> bool:
         fast = head
-        slow = head
-        while fast and fast.next: # is not null
+        slow = head # start the fast and slow at the same position, the next time they meet each other is the time we know we've detect a loop
+        while fast and fast.next: # is not null # also need to include fast.next beacuse we are shifting fast by two on each iteration
             fast = fast.next.next
             slow = slow.next
             if fast == slow:
                 return True
+        return False
+    
+  #hash table
+class Solution:
+    def hasCycle(self, head:ListNode) -> bool:
+        seen = set()
+        cur = head
+        while cur: # is not null
+            if cur in seen:
+                return True
+            seen.add(cur)
+            cur = cur.next
         return False
