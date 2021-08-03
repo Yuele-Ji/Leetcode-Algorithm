8aa2a743765f8fa319f833357f794e0d62378a79
Yuele-Ji <80063568+Yuele-Ji@users.noreply.github.com>
2021-08-03 01:05:29 -0400

    Update 34. Find First and Last Position of Element in Sorted Array.py

--- 34. Find First and Last Position of Element in Sorted Array.py
+++ 34. Find First and Last Position of Element in Sorted Array.py
@@ -27,7 +27,7 @@ class Solution:
     
     
     def findLeft(self, nums, target):
-        l, mid, r = 0, 0, len(nums)-1
+        l, mid, r = 0, 0, len(nums)-1 
         while l <= r:
             mid = l + (r-l)//2
             if nums[mid] < target:
@@ -46,3 +46,51 @@ class Solution:
             else:
                 r = mid - 1
         return r #比Target小的最大值
+
+class Solution:
+def searchRange(self,nums,target):
+    left = self.findleft(nums, target) # 最后写这里 let get the left by calling the findleft function, pass in nums, the target
+    right = self.findright(nums,target)
+    return [left,right] # and then we can return our left and right pointers
+                        # by default, if we can't find the result in the functions, it's going to return negative one
+
+def findleft(self,nums,target): # let's define our two helper functions, the first one is called find left, this will find the left boundary,
+                                  what we need to passed in is the array, and target
+    if not nums:
+        return -1
+    left = 0
+    right = len(nums) -1 # set the left and right to the beginning of the array and the end of the array
+
+    while left < right -1 : # we will keep going while our left is less than right minus 1
+        mid = (left +right) //2 # first we compute the mid index - left plus right divided by two, we use double slash here for integer division
+        if nums[mid] > target:
+            right = mid
+        if nums[mid] == target:
+            right = mid
+        if nums[mid] < target:
+            left = mid
+    if nums[left] ==target:
+        return left
+    if nums[right] ==target:
+        return right
+    return -1 # when this while loop ends, we didn't found anything, that means the target value does not exist, we'll just return -1
+
+def findright(self,nums,target):
+    if not nums:
+        return -1
+    left = 0
+    right = len(nums) - 1 # set the left and right to the beginning of the array and the end of the array
+
+    while left < right -1:
+        mid = (left + right) //2
+        if nums[mid] > target:
+            right = mid
+        if nums[mid] == target:
+            left = mid
+        if nums[mid] < target:
+            left = mid
+    if nums[right] == target:
+        return right
+    if nums[left] == target:
+        return left
+    return -1
